ee030f829903941b2575a0f1b8aa3980618f2aab
Bojan Bušljeta <bojan.busljetaexport NVM_DIR="$HOME/.nvm"bojan.busljeta@gmail.com>
2022-01-29 12:36:42 +0100

    add emotion

---
+++ styles/global.tsx
@@ -0,0 +1,7 @@
+import { css, Global } from '@emotion/react';
+import emotionNormalize from 'emotion-normalize';
+
+export const globalStyles = (
+    <Global styles={css`
+        ${emotionNormalize}
+`} />)
